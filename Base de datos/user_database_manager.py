6d0e8ac6acae05b8d061008856b4817f36668c77
Alfredo Aragon <u202210494@upc.edu.pe>
2023-09-20 15:04:03 -0500

    Agregada clase administradora para la base de datos

---
+++ Base de datos/user_database_manager.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+
+class SocialMediaDatabaseManager:
+    def __init__(self, database_name="social_media.db"):
+        self.conn = sqlite3.connect(database_name)
+        self.cursor = self.conn.cursor()
+
+    def __del__(self):
+        self.conn.close()
+
+    def add_user(
+        self,
+        username: str,
+        email: str,
+        password: str,
+        first_name: str,
+        last_name: str,
+        date_of_birth: str,
+        country: str,
+        phone_number: str,
+    ) -> bool:
+        try:
+            self.cursor.execute(
+                """
+                    INSERT INTO users (username, email, password, first_name, last_name, date_of_birth, country, phone_number)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                """,  # noqa: E501
+                (
+                    username,
+                    email,
+                    password,
+                    first_name,
+                    last_name,
+                    date_of_birth,
+                    country,
+                    phone_number,
+                ),
+            )
+            self.conn.commit()
+            return True
+
+        except sqlite3.IntegrityError:
+            return False
+
+    def remove_user(self, username):
+        try:
+            self.cursor.execute("DELETE FROM users WHERE username = ?", (username,))
+            self.conn.commit()
+            return True
+
+        except sqlite3.Error:
+            return False
