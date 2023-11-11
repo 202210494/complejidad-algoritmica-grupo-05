a41a133ec49c4e7e8570731eb65a9e74e2076f9a
Sebastian Valdivia Peceros <97813095+sebasvp2005@users.noreply.github.com>
2023-11-11 09:43:14 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,13 +1,26 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def init():
     return redirect(url_for("login"))
 
-@app.route("/login")
+@app.route("/login", methods = ["POST", "GET"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        redirect(url_for("home"))
+    else:
+        return render_template("login.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+@app.route("/user=<usr>")
+def profile(usr):
+    return render_template("profile.html")
+
+
 
 app.run(debug=True)
