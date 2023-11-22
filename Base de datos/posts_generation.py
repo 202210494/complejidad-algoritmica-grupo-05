ddaeaac6ce46165a763a9115d157199534945a06
Alfredo Aragon <u202210494@upc.edu.pe>
2023-11-21 21:27:58 -0500

    Agregado script generador de posts.

---
+++ Base de datos/posts_generation.py
@@ -0,0 +1,18 @@
+from faker import Faker
+import random
+
+from database_manager import SocialMediaDatabaseManager
+
+db_manager = SocialMediaDatabaseManager()
+fake = Faker()
+
+
+def generar(max_posts: int = 10) -> bool:
+    try:
+        for user in db_manager.get_all_users()[1]:
+            for _ in range(random.randint(0, max_posts)):
+                db_manager.add_post(username=user, content=fake.paragraph())
+        return True
+
+    except:  # noqa: E722
+        return False
