abb5f9c6670a1db505a0368edbd8d3a0c3d4fca2
Alfredo Aragon <u202210494@upc.edu.pe>
2023-11-21 21:54:02 -0500

    Unificados los scripts generadores en un solo script en

--- Base de datos/followers_generation.py
+++ Base de datos/followers_generation.py
@@ -3,10 +3,11 @@ import random
 from database_manager import SocialMediaDatabaseManager
 
 db_manager = SocialMediaDatabaseManager()
-all_users = db_manager.get_all_users()[1]
 
 
 def generar(max_follows: int = 50) -> bool:
+    all_users = db_manager.get_all_users()[1]
+
     try:
         for usuario in all_users:
             cantidad_a_seguir = random.randint(0, max_follows)
---
+++ Base de datos/generate_data.py
@@ -0,0 +1,10 @@
+import database_creation
+import user_generation
+import followers_generation
+import posts_generation
+
+
+assert database_creation.generar()
+assert user_generation.generar()
+assert followers_generation.generar()
+assert posts_generation.generar()
