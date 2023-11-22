76aea2188386f9c582b65d7e2c0e00c917ea66c5
Alfredo Aragon <u202210494@upc.edu.pe>
2023-11-21 21:36:26 -0500

    Actualizados los scripts de generacion de datos para funcionar como funciones.

--- Base de datos/followers_generation.py
+++ Base de datos/followers_generation.py
@@ -1,14 +1,22 @@
-from database_manager import SocialMediaDatabaseManager
 import random
+
+from database_manager import SocialMediaDatabaseManager
 
 db_manager = SocialMediaDatabaseManager()
 all_users = db_manager.get_all_users()[1]
 
-for usuario in all_users:
-    cantidad_a_seguir = random.randint(0, 50)
 
-    usuarios_a_seguir = random.sample(all_users, cantidad_a_seguir)
+def generar(max_follows: int = 50) -> bool:
+    try:
+        for usuario in all_users:
+            cantidad_a_seguir = random.randint(0, max_follows)
 
-    for seguido in usuarios_a_seguir:
-        if seguido != usuario:
-            db_manager.add_follow_relationship(usuario, seguido)
+            usuarios_a_seguir = random.sample(all_users, cantidad_a_seguir)
+
+            for seguido in usuarios_a_seguir:
+                if seguido != usuario:
+                    db_manager.add_follow_relationship(usuario, seguido)
+        return True
+
+    except:  # noqa: E722
+        return False
